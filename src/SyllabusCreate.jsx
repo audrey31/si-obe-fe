e5edfbb78dd6015d5ef9afff34599c1abdd5bfec
Audrey Axel Ristyanto <audrey.axel@suitmedia.com>
2023-12-20 23:22:15 +0700

    Fixed class naming into jsx format

--- src/SyllabusCreate.jsx
+++ src/SyllabusCreate.jsx
@@ -4,43 +4,55 @@ function SyllabusCreate() {
       <div className="container">
         <h1 className="fs-3">Create New Syllabus</h1>
         <nav aria-label="breadcrumb">
-          <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="/">Dashboard</a></li>
-            <li class="breadcrumb-item"><a href="/syllabus">Syllabus</a></li>
-            <li class="breadcrumb-item active" aria-current="page">create</li>
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <a href="/">Dashboard</a>
+            </li>
+            <li className="breadcrumb-item">
+              <a href="/syllabus">Syllabus</a>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              create
+            </li>
           </ol>
         </nav>
         <div className="card mb-5">
           <div className="card-body p-3">
             <div className="mb-3">
               <label htmlFor="input" className="form-label">
-                    Course
+                Course
               </label>
-              <select class="form-select" aria-label="Default select example">
+              <select
+                className="form-select"
+                aria-label="Default select example"
+              >
                 <option selected>Choose the course</option>
                 <option>...</option>
               </select>
             </div>
             <div className="mb-3">
               <label htmlFor="input" className="form-label">
-                    Course Code
+                Course Code
               </label>
               <input type="text" className="form-control" id="input" />
             </div>
             <div className="mb-3">
               <label htmlFor="input" className="form-label">
-                    Author
+                Author
               </label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
             </div>
             <div className="mb-3">
               <label htmlFor="input" className="form-label">
-                    Head of Study Program
+                Head of Study Program
               </label>
               <input type="text" className="form-control" id="input" />
             </div>
-            
-            
+
             <div className="d-flex mt-5">
               <button type="submit" className="btn btn-primary me-2">
                 Save
@@ -53,11 +65,6 @@ function SyllabusCreate() {
         </div>
       </div>
     </main>
-  
-  
-  
-  
-  
   );
 }
 
