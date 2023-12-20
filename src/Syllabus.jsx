2f0cec2d5d34c0925e9f346b257d7197fb22621d
claireYng <claraclaritayng@gmail.com>
2023-12-20 20:11:29 +0700

    Update Syllabus.jsx

--- src/Syllabus.jsx
+++ src/Syllabus.jsx
@@ -3,9 +3,39 @@ function Syllabus() {
     <main className="pt-4">
       <div className="container">
         <h1 className="fs-3">My Syllabus</h1>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Syllabus</li>
+          </ol>
+        </nav>
+        <div class="float-end">
+          <button type="button" class="btn btn-outline-primary my-3">Create new Syllabus</button>
+        </div>
         
-
-
+        <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Course</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button type="button" class="btn me-md-2">Edit</button>
+              <button type="button" class="btn text-danger">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
       </div>
     </main>
   );
